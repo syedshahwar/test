4d1cd73df40ed50f11910d1791936a2826000316
syedshahwar <syed.shahwar7@gmail.com>
2022-01-10 18:20:11 +0500

    test

---
+++ lib.js
@@ -0,0 +1,21 @@
+class Test {
+  #logger;
+
+  /**
+   * 
+   * @param {string} id
+   */
+  init(id) {
+    if (!this.#logger) {
+      this.#logger = {id: id};
+    }
+  }
+
+  async log() {
+
+    console.log(`id -> `, this.#logger.id)
+  }
+
+}
+
+module.exports = new Test()
